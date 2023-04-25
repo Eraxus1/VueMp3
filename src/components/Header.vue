d7ae564db815d4b97ed12c53f3a3afca6f707d59
Oğuz Can <107717374+Eraxus1@users.noreply.github.com>
2023-04-25 18:22:47 +0300

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <router-link to="/about">Music Type</router-link>
+    <router-link to="/about">Music Type</router-link>
+    <router-link to="/about">Music Type</router-link>
+    <RouterView />
+  </header>
+</template>
+
+<style>
+header{
+  padding: 10px;
+  text-align: center;
+  border-bottom: 1px solid gray;
+}
+a{
+  color: white;
+  padding: 1rem;
+  margin: 10px;
+  border-radius: 5px;
+  text-decoration: none;
+}
+
+</style>
+
+<script>
+</script>
