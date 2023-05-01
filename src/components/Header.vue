d43b4b0836b1e61fd2cbc002214f6a2570a78ea1
Oğuz Can <107717374+Eraxus1@users.noreply.github.com>
2023-05-02 01:35:28 +0300

    Add files via upload

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,76 @@
 <template>
-  <header>
+  <div class="headerbuttons">
+    <button class="toggle" @click="toggle()">Open The Bar</button>
+  </div>
+  <header v-if="show">
     <router-link to="/about">Music Type</router-link>
     <router-link to="/about">Music Type</router-link>
     <router-link to="/about">Music Type</router-link>
     <RouterView />
   </header>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show
+    }
+  }
+}
+</script>
 
 <style>
-header{
-  padding: 10px;
-  text-align: center;
-  border-bottom: 1px solid gray;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-a{
+
+header {
+  width: 100%;
+  border: 1px solid white;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  padding: 10px;
+}
+
+
+
+.toggle {
+  width: 10%;
+  height: 30px;
+  border: none;
+  background: none;
+  box-shadow: 0 0 2px white, 0 0 10px #053754;
   color: white;
-  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin: 10px;
-  border-radius: 5px;
+  margin-left: 44.5%;
+  border-radius: 10px;
+
+}
+
+a {
+  color: white;
+  margin: 15px;
   text-decoration: none;
 }
 
+@media (max-width:500px) {
+  .toggle {
+    width: 25%;
+    margin-left: 37%;
+  }
+}
 </style>
 
-<script>
-</script>
+
